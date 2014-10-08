728f0e29f8f044d1448732fe297644d1bd13d903
thierry <thierry.rakotoarivelo@nicta.com.au>
2014-10-08 17:01:59 +1100

    Updated ping app def for OMF6

--- omf/ping-oml2.app.rb
+++ omf/ping-oml2.app.rb
@@ -9,12 +9,11 @@
 #
 defApplication('oml:app:ping', 'ping') do |a|
 
-  a.version(2, 9, 0)
-  a.shortDescription = "Wrapper around ping"
+  # a.version(2, 9, 0) # Deprecated in OMF6
   a.description = %{This application runs the system ping, parses its output and
 reports the measurements via OML
   }
-  a.path = "/usr/local/bin/ping-oml2"
+  a.binary_path = "/usr/local/bin/ping-oml2"
 
   a.defProperty('dest_addr', 'Address to ping', nil,
 		:type => :string)
